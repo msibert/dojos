58f78eb3096dc3b46471b1a6de063855afc8351d
Maximiliano Sibert <maximiliano.sibert@mercadolibre.com>
2018-06-22 00:31:36 -0300

    initial commit

---
+++ mvn_base/src/main/java/com/mercadolibre/dojos/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.dojos;
+
+/**
+ * Mercado Libre - CHO
+ * Created on 22/6/18.
+ */
+public class HelloWorld {
+
+    public String sayHello(){
+        return "Hello [waving hand]";
+    }
+}
